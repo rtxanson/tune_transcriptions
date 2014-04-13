f2bea7785b925f3da984e677bea1e70e4a3d19b2
Ryan <ryan.txanson@gmail.com>
2014-04-13 18:14:38 -0500

    notes

--- accompaniment/misc/chord_sort.py
+++ accompaniment/misc/chord_sort.py
@@ -58,6 +58,16 @@ class Tune(object):
         type, _, sig = l.partition(' in ')
         return type, sig
 
+    def uniqued(self, parts):
+        # TODO: remove repeat symbol (:) and sort in order of descending
+        # frequency.
+
+        _all = []
+        for k, v in parts.iteritems():
+            _all.extend( list(set(sum(v, []))) )
+
+        return list(set(_all))
+
     def __init__(self, chunk):
         self.raw_chunk = [l for l in chunk if l != _TUNE_DELIM]
 
@@ -65,6 +75,7 @@ class Tune(object):
         self.type, self.key = self.process_type_sig(self.raw_chunk[1])
 
         self.parts = self.split_parts(self.raw_chunk[2::])
+        self.unique_chords = self.uniqued(self.parts)
 
 def chunk_tunes(lines):
     chunks, chunk = [], []
@@ -89,31 +100,77 @@ def main():
                  if l.startswith('    ')
                 ]
 
-    print 'lines: %d' % len(lines)
+    print >> sys.stdout, 'lines: %d' % len(lines)
     tunes = map(Tune, chunk_tunes(lines))
-    print 'tunes: %d' % len(tunes)
-    print
-    print
+    print >> sys.stdout, 'tunes: %d' % len(tunes)
+    print >> sys.stdout, ''
+    print >> sys.stdout, ''
 
     # TODO: grab all the keys, and sort by largest, then start to figure
     # out common progressions
 
     tunes_g = [t for t in tunes if t.key == 'D' and t.type == 'Reel']
+    tune_types = list(set((t.type for t in tunes)))
 
-    print len(tunes_g)
+    filtered_types = [
+        'Jig',
+        'Polka',
+        'Barndance',
+        'Reel',
+        'Rel',
+        'Slip Jig',
+        'Slip jig',
+    ]
 
     from collections import defaultdict
 
-    first_a_parts = defaultdict(list)
+    for _type in filtered_types:
+        _keys_for_tune_type = list(set((t.key for t in tunes if t.type == _type)))
 
-    for g in tunes_g:
-        first_a_parts[ '|'.join([':'.join(b) for b in g.parts.get('A')]) ].append(g.title)
+        for k in _keys_for_tune_type:
 
-    for k, v in first_a_parts.iteritems():
-        if len(v) > 1:
-            print k
-            for n in v:
-                print '  - ' + n
+            tunes_in_key = [t for t in tunes if t.key == k and t.type == _type]
+            print >> sys.stdout, ''
+            print >> sys.stdout, _type, k, "(%d tunes)" % len(tunes_in_key)
+
+            all_chords = defaultdict(list)
+            for t in tunes_in_key:
+                # TODO: order unique_chords by chord frequency in tune
+                all_chords[ '|'.join(t.unique_chords) ].append(t.title)
+
+            for k, v in all_chords.iteritems():
+                if len(v) > 1:
+                    print >> sys.stdout, '    ' + k
+                    for n in v:
+                        print >> sys.stdout, '      - ' + n
+
+
+
+
+    # print len(tunes_g)
+
+
+    # first_a_parts = defaultdict(list)
+
+    # for g in tunes_g:
+    #     first_a_parts[ '|'.join([':'.join(b) for b in g.parts.get('A')]) ].append(g.title)
+
+    # for k, v in first_a_parts.iteritems():
+    #     if len(v) > 1:
+    #         print k
+    #         for n in v:
+    #             print '  - ' + n
+
+    # all_chords = defaultdict(list)
+
+    # for g in tunes_g:
+    #     all_chords[ '|'.join(g.unique_chords) ].append(g.title)
+
+    # for k, v in all_chords.iteritems():
+    #     if len(v) > 1:
+    #         print k
+    #         for n in v:
+    #             print '  - ' + n
 
 
 
